c6d8530e344a73d2b0a3518dce7b8465c09386b6
jeondaen <jeondaen@gmail.com>
2020-05-17 17:15:44 +0900

    specifiy the return value and rename parameter name

--- exercise_4/where.dart
+++ exercise_4/where.dart
@@ -1,7 +1,7 @@
-var keepEvenNumbers = (Iterable<int> iter) {
-  return iter.where((element) => element.isEven);
-};
+Iterable<int> keepEvenNumbers(Iterable<int> numbers) {
+  return numbers.where((element) => element.isEven);
+}
 
 main(List<String> args) {
-  print(keepEvenNumbers([1, 2, 3]));
+  print(keepEvenNumbers([1, 2, 3, 4]));
 }
