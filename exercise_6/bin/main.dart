f90618d093fffd79ab2067ddb21e04c65727e5f6
jeondaen <jeondaen@gmail.com>
2020-05-24 16:00:35 +0900

    exercise_6

---
+++ exercise_6/bin/main.dart
@@ -0,0 +1,20 @@
+import 'package:http/http.dart' as http;
+import 'package:html/parser.dart' show parse;
+import 'package:html/dom.dart';
+
+var url = "https://example.com";
+
+Future<String> getHttp() async {
+  var html = await http.read(url);
+  return html;
+}
+void main(List<String> arguments) async {
+
+  var html = await getHttp();
+  
+  var document = parse(html);
+  var title = document.querySelector("title").innerHtml;
+
+  print(title);
+
+}
