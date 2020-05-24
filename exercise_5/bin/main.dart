04ce685a5acfba2ad2bff49552137b9336ff5aae
jeondaen <jeondaen@gmail.com>
2020-05-24 16:03:18 +0900

    real last version

--- exercise_5/bin/main.dart
+++ exercise_5/bin/main.dart
@@ -3,17 +3,11 @@ import 'package:http/http.dart' as http;
 var url = 'https://example.com/';
 
 Future<String> getHttp() async {
-  var response = await http.post(url, body: {'name' : 'doodle', 'color' : 'blue'});
-  
-  print('Response status: ${response.statusCode}');
-  print('Response body: ${response.body}');
 
-  print(await http.read(url));
-
-  return null;
+  return await http.read(url);
 }
 
-void main(List<String> arguments) {
-  getHttp();
+void main(List<String> arguments) async {
+  print(await getHttp());
 }
 
